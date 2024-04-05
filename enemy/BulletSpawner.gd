bc6b435097282e1162c47b95a663185d5d3c6079
Ivo Pačnik <pacnik@gmail.com>
2024-04-05 19:31:18 +0200

    Enemy is now a straight shooter

--- enemy/BulletSpawner.gd
+++ enemy/BulletSpawner.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
-@export var BULLET_SPEED : int = 25
-@onready var bullet_scene = preload("res://standard_bullet.tscn")
+@export var BULLET_SPEED : float = 25
+@onready var bullet_scene = preload("res://enemy/enemy_bullet.tscn")
 @onready var bullet_spawn_point = %EnemyBulletSpawner
 
 # Called when the node enters the scene tree for the first time.
@@ -11,14 +11,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	spawn_bullet()
 	pass
 
+func _physics_process(_delta):
+	var _directionOfEnemy = (Global.player.position- position).normalized()
+	spawn_bullet()
+	
 func spawn_bullet():
 	var projectile = bullet_scene.instantiate()
 	add_sibling(projectile)
-	projectile.transform = bullet_spawn_point.global_transform
-	projectile.linear_velocity = bullet_spawn_point.global_transform.basis.z * -1 * BULLET_SPEED
+	projectile.global_position = bullet_spawn_point.global_position
+	projectile.linear_velocity = bullet_spawn_point.global_transform.basis.z * BULLET_SPEED
 
 func _on_despawning_bullets_timeout():
 	queue_free()
